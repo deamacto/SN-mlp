a1872aaa21dcace6afaefbbd5faef0b63fd5ed53
Deamacto <pszon.joanna@gmail.com>
2022-11-20 02:20:17 +0100

    added report

--- src/main/java/Consts.java
+++ src/main/java/Consts.java
@@ -1,11 +1,11 @@
 public class Consts {
-    public static final double DESIRED_STANDARD_DEVIATION = 0.01;
+    public static final double DESIRED_STANDARD_DEVIATION = 0.1;
     public static final double DESIRED_MEAN = 0.0;
 
     public static final int NEURONS_INPUT = 784;
     public static final int NEURONS_OUTPUT = 10;
 
-    public static final int BATCH_SIZE = 60;
+    public static final int BATCH_SIZE = 50;
     public static final double ALPHA = 0.6;
     public static final double ALPHA_BY_BATCH = ALPHA/BATCH_SIZE;
 }
